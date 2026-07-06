33c8da8539097775b12ee2883264f14bac54e919
PythonDeveloper2026 <kamtarin.odam@gmail.com>
2026-07-06 09:38:55 +0500

    kerakli packagelar royhatini tuzib qoydik

---
+++ prosta.py
@@ -0,0 +1,10 @@
+# git init - loyihani gitga qoshish
+
+# git add . - kodlarni gitga saqashga tayyorlaydi.
+
+# git commit -m "izoh" - gitga saqlash
+
+
+# git status - gitning holatini ya'ni qaysi bosqichdaligini korgani
+
+# git log - tarixdagi saqlanishlarni korish uchun
